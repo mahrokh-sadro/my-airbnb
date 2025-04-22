dfac708fe7056a2e52137d4c91a813d74ac7f058
mahrokh sadro <mahrokh.sadro@j-sas.com>
2025-04-22 11:36:18 -0400

    update eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,13 +14,7 @@ const eslintConfig = [
   {
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        {
-          argsIgnorePattern: "^_",
-          varsIgnorePattern: "^_",
-        },
-      ],
+      "@typescript-eslint/no-unused-vars": "off",
     },
   },
 ];
